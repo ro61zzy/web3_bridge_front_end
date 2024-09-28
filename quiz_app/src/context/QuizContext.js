67d6b1b0e4d7a97cb9e78b0cf326349f35175486
rozzy <rosekaremeri@gmail.com>
2024-09-28 18:47:33 +0300

    useContext Provider

--- quiz_app/src/context/QuizContext.js
+++ quiz_app/src/context/QuizContext.js
@@ -1,3 +1,32 @@
 import React, { createContext, useState } from 'react';
 
 const QuizContext = createContext();
+
+const QuizProvider = ({ children }) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const questions = [
+    {
+      question: 'What is the capital of France?',
+      options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+      answer: 'Paris',
+    },
+   
+  ];
+
+
+
+  return (
+    <QuizContext.Provider
+      value={{
+      
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+};
+
+export { QuizContext, QuizProvider };
